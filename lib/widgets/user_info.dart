759190bd81e9a068b6344eb5d71b883682d30361
Volodymyr <vmzagasailo@gmail.com>
2021-01-22 18:52:14 +0200

    rebuild user_info widget

--- lib/widgets/user_info.dart
+++ lib/widgets/user_info.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
-class Infouser extends StatelessWidget {
-  Infouser(
-    this.userImage,
-    this.userName,
-    this.userEmail,
-  );
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-  final String userName;
-  final String userImage;
-  final String userEmail;
+class InfoUser extends StatelessWidget {
+  Future _getUserId() async {
+    final user = await FirebaseAuth.instance.currentUser();
+
+    return user;
+  }
+
+  Future _getUserInfo() async {
+    final user = await FirebaseAuth.instance.currentUser();
+    final userData =
+        await Firestore.instance.collection('users').document(user.uid).get();
+
+    return userData;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          radius: 35,
-          backgroundImage: NetworkImage(userImage),
-        ),
-        Text(
-          userName,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        Text(userEmail),
-      ],
-    );
+    return FutureBuilder(
+        future: _getUserId(),
+        builder: (ctx, futureSnapshot) {
+          return FutureBuilder(
+            future: _getUserInfo(),
+            builder: (ctx, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 35,
+                    backgroundImage: NetworkImage(
+                      userSnapshot.data['image_url'],
+                    ),
+                  ),
+                  Text(
+                    userSnapshot.data['username'],
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(userSnapshot.data['email']),
+                ],
+              );
+            },
+          );
+        });
   }
 }
