a2bf440937be26d80d8c6b693d6e024bd6b82fa9
Volodymyr <vmzagasailo@gmail.com>
2021-01-22 18:02:39 +0200

    fixed bug in rgb generator

--- lib/theme/colors_generator.dart
+++ lib/theme/colors_generator.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 mixin ColorsGenerator {
-  final Random _random = Random();
-
+  Random _random = Random();
+  
   Color get randomColor {
-    return Color.fromRGBO(
-        _random.nextInt(255), _random.nextInt(255), _random.nextInt(255), 1);
+    return Color.fromRGBO(_random.nextInt(256), _random.nextInt(256),
+        _random.nextInt(256), 1);
   }
 }
